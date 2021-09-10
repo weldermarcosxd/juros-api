0b4d0bef3bcb4b0c585c50ad918c67e136fbd11f
welder marcos <weldermarcosxd@gmail.com>
2021-09-10 07:05:25 -0300

    commit api base taxa juros

---
+++ TaxaJurosService/Controllers/TaxaJurosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TaxaJurosService.Controllers
+{
+    [ApiController]
+    [Route("taxaJuros")]
+    public class TaxaJurosController : ControllerBase
+    {
+        private readonly ILogger<TaxaJurosController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public TaxaJurosController(ILogger<TaxaJurosController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public decimal GetAsync()
+        {
+            _logger.LogInformation("obtendo taxa de juros");
+            return _configuration.GetValue<decimal>("PercentualJuros");
+        }
+    }
+}
