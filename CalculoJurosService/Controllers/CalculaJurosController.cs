074a52d0233fbb00f16d322ae1b08f3d19845b17
welder marcos <weldermarcosxd@gmail.com>
2021-09-10 07:42:51 -0300

    base servico calculo juros

---
+++ CalculoJurosService/Controllers/CalculaJurosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace CalculoJurosService.Controllers
+{
+    [ApiController]
+    [Route("calculaJuros")]
+    public class CalculaJurosController : ControllerBase
+    {
+        private readonly ILogger<CalculaJurosController> _logger;
+
+        public CalculaJurosController(ILogger<CalculaJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<double> Get([FromQuery] decimal valorInicial, [FromQuery] int meses)
+        {
+            _logger.LogInformation("Calculando juros");
+            var x = (double)valorInicial * Math.Pow(1 + 0.01, meses);
+            return Math.Round(x, 2);
+        }
+    }
+}
